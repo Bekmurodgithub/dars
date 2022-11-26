337d303edb9939f6d177225ac68b8b5ccda96f03
Islomjon Makhamadjanov <islomiy1101@gmail.com>
2022-11-26 17:47:52 +0500

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,34 @@
                   </tr>
                 </thead>
                 <tbody>
-                  
+                  <?php
+                    $query="SELECT * FROM students ORDER BY student_id";
+                    $query_run=mysqli_query($con,$query);
+
+                    if(mysqli_num_rows($query_run)>0){
+                      foreach($query_run as $student){
+                        // echo $student['student_name'];
+                        ?>
+                        <tr>
+                          <td><?= $student['student_id']?></td>
+                          <td><?= $student['student_name']?></td>
+                          <td><?= $student['student_email']?></td>
+                          <td><?= $student['student_phone']?></td>
+                          <td><?= $student['student_course']?></td>
+                          <td>
+                            <a href="student_view.php?id=<?= $student['student_id']?>" class="btn btn-info btn-sm">View</a>
+                            <a href="student_edit.php?id=<?= $student['student_id']?>" class="btn btn-warning btn-sm">Edit</a>
+                            <form action="code.php" method="POST" class="d-inline">
+                              <button type="submit" class="btn btn-danger btn-sm" value="<?= $student['student_id'];?>" name="deleteBtn">Delete</button>
+                            </form>
+                          </td>
+                        </tr>
+                        <?php
+                      }
+                    }else{
+                      echo "<h4 class='text-center'>Hozircha birorta xam student ma'lumoti yo'q</h4>";
+                    }
+                  ?>
                 </tbody>
               </table>
             </div>
